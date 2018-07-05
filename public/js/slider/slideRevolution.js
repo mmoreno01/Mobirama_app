38db2da734599374707269373ae9b47b574652bd
MIGUEL ANGEL MORENO <you@example.com>
2018-07-05 17:07:16 -0500

    creacion de slider revolution

---
+++ public/js/slider/slideRevolution.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function() {
+       jQuery('.tp-banner').revolution(
+        {
+            delay:9000,
+            startwidth:1170,
+            startheight:500,
+            hideThumbs:10
+        });
+});
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,15 +7,19 @@
     <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
     <!-- estilo carousel -->
     <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <!-- slider revolution -->
+    <link rel="stylesheet" href="{{asset('css/settings.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
     <!-- slyles -->
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <!-- icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
-
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.js"></script>
 
     <!-- menu principal -->
-    <header class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top animated fadeInDown">
+    <header class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top wow fadeIn" data-wow-duration="1s" data-wow-delay="1s">
     <a class="navbar-brand" href="/">
         <img src="imagenes/home-icons/jblogo.png" width="30" height="30" alt="">
     </a>      
@@ -26,7 +30,7 @@
         <div class="navbar-nav mr-auto ml-auto">
             <div class="row contentPricipal">
                 <div class="col-md-9 content_serv">
-                <div class="row">
+                <div class="row wow slideInLeft" data-wow-duration="2s" data-wow-delay="5s">
                         <div class="col-4 content_img">
                             <img src="imagenes/house-loan-opportunity_page.png.img.320.1519851211549.png" alt="">
                             <h5>Titles</h5>
@@ -43,7 +47,7 @@
                             <p>Lorem ipsum dolors! Dolorem ipsum debiti</p>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row wow slideInLeft" data-wow-duration="2s" data-wow-delay="5s">
                         <div class="col-4 content_img">
                             <img src="imagenes/house-loan-opportunity_page.png.img.320.1519851211549.png" alt="">
                             <h5>Titles</h5>
@@ -80,7 +84,7 @@
         </div>-->
     </div>    
 </header>
-    <!-- fin del menu principal -->
+<!-- fin del menu principal -->
     <title>Document</title>
 </head>
 <body>
@@ -112,18 +116,34 @@
     </div>
 </footer>
 
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<!-- <script src="js/jquery.js"></script>   -->
     <script src="js/jquery-3.3.1.min.js"></script>
+
+ 
     <script src="js/bootstrap.min.js"></script>
     <!-- carousel service -->
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/carousel.js"></script>
     <!-- animate js -->
     <script src="js/wow.min.js"></script>
+    <!-- slider revolution -->
+    <script src="js/jquery.themepunch.revolution.js"></script>
+    <script src="js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/jquery.themepunch.plugins.min.js"></script>
+    <script src="js/slider/slideRevolution.js"></script>
+    <!-- animate -->
     <script>
-        new WOW().init();
-    </script>
+        wow = new WOW(
+                        {
+                        boxClass:     'wow',      // default
+                        animateClass: 'animated', // default
+                        offset:       0,          // default
+                        mobile:       true,       // default
+                        live:         true        // default
+                        }
+                        )
+        wow.init();    
+    </script> 
     <!-- vue js -->
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </body>
