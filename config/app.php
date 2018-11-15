9a63b0a3b4e5632051c6df75d57c7a4e983c6169
mmoreno01 <migue.moreno01@gmail.com>
2018-11-15 15:19:29 -0600

    prueba de database en heroku

--- config/app.php
+++ config/app.php
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:MCO3sCd1iycI6JZ+cs1jfRxSUg7mKUXYgOgwnAGZcgI='),
 
     'cipher' => 'AES-256-CBC',
 
--- config/database-back.php
+++ config/database-back.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('ec2-50-19-249-121.compute-1.amazonaws.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd5d85m3fmd61q8'),
-            'username' => env('DB_USERNAME', 'wjvmpaqbjvyvaq'),
-            'password' => env('DB_PASSWORD', '72b30c4ef1f9827332079c4f6be2614bb6e5aeeff128ba197171828a13d74ca6'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
