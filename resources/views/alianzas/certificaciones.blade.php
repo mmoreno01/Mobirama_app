d2909dbc20307e05cdede30c193987316bd820e5
mmoreno01 <migue.moreno01@gmail.com>
2018-11-21 08:57:39 -0600

    creacion de grid de certificaciones

--- resources/views/alianzas/certificaciones.blade.php
+++ resources/views/alianzas/certificaciones.blade.php
@@ -7,4 +7,41 @@
         <h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical" uk-parallax="y: 100,0">Certificaciones</h1>
     </div>
 <!-- fin de la imagen principal -->
+
+<section id="content-img-certifications">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <img src="imagenes/home-icons/iso9001.png" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">ISO 9001</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi voluptate, magni libero quidem optio numquam incidunt placeat reprehenderit</p>
+                            <a href="newsletter/Mobirama.pdf" class="btn btn-primary"   target="_blank" >Ver más</a>
+                        </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                    <div class="card">
+                            <img src="imagenes/home-icons/GPTW.png" class="card-img-top" alt="">
+                                <div class="card-body">
+                                    <h5 class="card-title">Great Place To Work</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi voluptate, magni libero quidem optio numquam incidunt placeat reprehenderit</p>
+                                    <a href="newsletter/Mobirama.pdf" class="btn btn-primary"   target="_blank" >Ver más</a>
+                                </div>
+                        </div>
+            </div>
+            <div class="col-md-4">
+                    <div class="card">
+                            <img src="imagenes/home-icons/50Consultorias.png" class="card-img-top" alt="">
+                                <div class="card-body">
+                                    <h5 class="card-title">50 Consultorias</h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi voluptate, magni libero quidem optio numquam incidunt placeat reprehenderit</p>
+                                    <a href="newsletter/Mobirama.pdf" class="btn btn-primary"   target="_blank" >Ver más</a>
+                                </div>
+                        </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
