a568b6c6d608e7517b86f667cb9d0500d5f381eb
mmoreno01 <migue.moreno01@gmail.com>
2018-11-21 18:02:49 -0600

    cambio de imgan 50consultorias2

--- resources/views/alianzas/certificaciones.blade.php
+++ resources/views/alianzas/certificaciones.blade.php
@@ -29,11 +29,11 @@
                                     <p class="card-text">Nos ubicamos dentro del grupo de las 100 mejores empresas para trabajar en el país.</p>
                                     <!-- <a href="newsletter/Mobirama.pdf" class="btn btn-primary"   target="_blank" >Ver más</a> -->
                                 </div>
-                        </div>
+                    </div>
             </div>
             <div class="col-md-4">
                     <div class="card">
-                            <img src="imagenes/home-icons/Distintivo RH 2018.png" width="286px"  height="180px" class="card-img-top" alt="">
+                            <img src="imagenes/home-icons/50Consultorias.png" width="286px"  height="180px" class="card-img-top" alt="">
                                 <div class="card-body">
                                     <h5 class="card-title">50 Consultorias</h5>
                                     <p class="card-text">A lo largo de nuestra trayectoria hemos trabajado para las mejores empresas en el país, siendo reconocidos como una de las 50 empresas de consultoría de México.</p>
