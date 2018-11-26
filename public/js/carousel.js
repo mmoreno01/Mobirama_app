068eb0b21e63a67566d1a3a36b60101133994db7
mmoreno01 <migue.moreno01@gmail.com>
2018-11-26 10:58:17 -0600

    se cambio la velociad  slider en servicrds

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -7,7 +7,7 @@
         autoplay,
         autoplayTimeout:7000,
         autoplayHoverPause:true,
-        autoplaySpeed:7000, 
+        autoplaySpeed:9000, 
         responsive: {
             0: {
                 items :1
