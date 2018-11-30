b1fc02e6753f3fce20961830fd68c363d80f7eac
mmoreno01 <migue.moreno01@gmail.com>
2018-11-30 12:01:51 -0600

    cambio de estilo en home

--- resources/views/nosotros/nosotros.blade.php
+++ resources/views/nosotros/nosotros.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 <!-- imagen principal -->
-<div class=" parallax uk-height-large uk-background-cover uk-light uk-flex uk-flex-top" uk-parallax="bgy: -200" style="background-image: url('imagenes/Master-Servicios-assets/Nosotros.jpg');">
+<div class=" parallax uk-height-large uk-background-cover uk-light uk-flex uk-flex-top" uk-parallax="bgy: -30s0" style="background-image: url('imagenes/Master-Servicios-assets/Nosotros.jpg');">
     <div class="overlay"></div>
-    <h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical" uk-parallax="y: 100,0">Nosotros</h1>
+    <h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical" uk-parallax="y: 10,0">Nosotros</h1>
 </div>
 <!-- fin de la imagen principal -->
 
