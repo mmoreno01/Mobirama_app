ab04ffca291824007c9559132b8f8f1bfaa55858
mmoreno01 <migue.moreno01@gmail.com>
2018-12-14 16:38:46 -0600

    cambio de alert secces closed

--- resources/views/common/success.blade.php
+++ resources/views/common/success.blade.php
@@ -1,7 +1,13 @@
 
 @if(session('info'))
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+   {{session('info')}}
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
     <div class="alert alert-success" role="alert">
-        {{session('info')}}
+       
     </div>
 @endif   
 
