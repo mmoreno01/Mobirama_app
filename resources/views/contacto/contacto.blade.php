363b3a48352917b7bc30c3cb1371c026b9d517f1
mmoreno01 <migue.moreno01@gmail.com>
2018-12-14 15:59:44 -0600

    cambio de valiodaciones en  contacto

--- resources/views/contacto/contacto.blade.php
+++ resources/views/contacto/contacto.blade.php
@@ -13,23 +13,31 @@
                 <div class="row">
                         <div class="col-md-8  content-form">
                                  @include('common.success')
-                                 @include('common.errors')
+                                 {{-- @include('common.errors') --}}
                                 {!! Form::open(['route' => 'contacto.contacto', 'method' => 'POST' ]) !!}
                                 @csrf
                                 <div class="row">
                                         <div class="col-lg-6">
                                              <div class="form-group">
                                                 {!! Form::text('nombre', null, [  'class' => 'form-control inp-text ', 'placeholder' => 'Nombre', ] ) !!}
+                                                {!! $errors->first('nombre', '<span class="help-block alert-danger">:message</span>') !!}
+
                                              </div>
                                              <div class="form-group">
                                                 {!! Form::email('email', null, [  'class' => 'form-control inp-text', 'placeholder' => 'Correo electrónico',] ) !!}
+                                                {!! $errors->first('email', '<span class="help-block alert-danger">:message</span>') !!}
+
                                              </div>
                                              <div class="form-group">
                                                 {!! Form::text('telefono', null, [  'class' => 'form-control inp-text', 'placeholder' => 'Teléfono',] ) !!}
+                                                {!! $errors->first('telefono', '<span class="help-block alert-danger">:message</span>') !!}
+
                                              </div>
                                         </div>
                                         <div class="col-lg-6 form-group">
                                                 {!! Form::textarea('mensaje', null, [ 'class' => 'form-control style-textarea', 'placeholder' => 'Mensaje',] ) !!}
+                                                {!! $errors->first('mensaje', '<span class="help-block alert-danger">:message</span>') !!}
+
                                         </div> 
 
                                         <div class="col-md-12 form-group">
