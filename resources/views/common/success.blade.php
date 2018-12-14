eb071cd486229802ded9ddcd118d72076e6d03a5
mmoreno01 <migue.moreno01@gmail.com>
2018-12-14 16:43:06 -0600

    cambio de alert secces closed color

--- resources/views/common/success.blade.php
+++ resources/views/common/success.blade.php
@@ -1,13 +1,10 @@
 
 @if(session('info'))
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
+<div class="alert alert-success alert-dismissible fade show" role="alert">
    {{session('info')}}
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
-    <div class="alert alert-success" role="alert">
-       
-    </div>
 @endif   
 
